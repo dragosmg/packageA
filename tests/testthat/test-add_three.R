645cbabb25e4b34b3b05c04fbadb68ae71b3da54
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2023-11-15 10:07:02 +0000

    solved lint and commented out the error tests to trigger test coverage

--- tests/testthat/test-add_three.R
+++ tests/testthat/test-add_three.R
@@ -9,24 +9,23 @@ test_that("add_three() works with numeric inputs", {
 })
 
 test_that("add_three() complains with non-numeric inputs", {
-  expect_error(
-    add_three("a"),
-    "`add_three()` expects a numeric input. You have supplied a character.",
-    fixed = TRUE
-  )
-  expect_error(
-    add_three(TRUE),
-    "`add_three()` expects a numeric input. You have supplied a logical.",
-    fixed = TRUE
-  )
-  expect_error(
-    add_three(c("2", "5")),
-    "`add_three()` expects a numeric input. You have supplied a character.",
-    fixed = TRUE
-  )
-
-  expect_snapshot_error(add_three("a"))
-  expect_snapshot_error(add_three(TRUE))
-  expect_snapshot_error(add_three(c("2", "5")))
+  # expect_error(
+  #   add_three("a"),
+  #   "`add_three()` expects a numeric input. You have supplied a character.",
+  #   fixed = TRUE
+  # )
+  # expect_error(
+  #   add_three(TRUE),
+  #   "`add_three()` expects a numeric input. You have supplied a logical.",
+  #   fixed = TRUE
+  # )
+  # expect_error(
+  #   add_three(c("2", "5")),
+  #   "`add_three()` expects a numeric input. You have supplied a character.",
+  #   fixed = TRUE
+  # )
+  #
+  # expect_snapshot_error(add_three("a"))
+  # expect_snapshot_error(add_three(TRUE))
+  # expect_snapshot_error(add_three(c("2", "5")))
 })
-
