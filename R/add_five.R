a233ea9cdce521847b39af2deef4497fb023a8ae
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2025-09-05 16:11:45 +0100

    add five

---
+++ R/add_five.R
@@ -0,0 +1,21 @@
+#' Add five to a numeric vector
+#'
+#' @inheritParams add_one
+#'
+#' @return a numeric vector
+#' @export
+#'
+#' @examples
+#' add_five(2)
+#' add_five(3)
+add_five <- function(x) {
+    if (!is.numeric(x)) {
+    cli::cli_abort(
+      "{.fn add_five} expects a numeric input. You have supplied a {.obj_type_friendly x}."
+    )
+  }
+
+  x %>%
+    add_two() %>%
+    add_three()
+}
